d87dc5998b5903c93619a5632bb9e3d4650f5380
Vamsi <bharathvamsivukoti@gmail.com>
2024-09-12 15:10:44 +0530

    gfhgjh

---
+++ Index.js
@@ -0,0 +1,42 @@
+// {
+//   /* <script>
+//     let a=10;
+//     console.log("a=20.60"+typeof a);
+//     let f="tru";
+//     console.log(f+)
+// </script> */
+// }
+
+// const student = {
+//   name: "Brad",
+//   marks: "100",
+// };
+// student.marks = 200;
+
+// console.log(Object.entries(student));
+// const colors = ["red", "blue"];
+
+// console.log(colors[1]);
+
+// colors[1] = "Yellow";
+// // console.log(colors[1])
+
+// colors.push("Black");
+// console.log(colors);
+
+// colors.unshift("Purple")
+
+// function greet(nam){
+//     console.log(nam)
+// }
+// greet("Brad")
+
+function sum1(a,b){
+    return a+b;
+}
+
+const sum= (a,b) => a+b
+console.log(sum(1,2));
+console.log(sum(1,2));
+
+
